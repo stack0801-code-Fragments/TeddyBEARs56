abe389a6a191cf8e5a9d2919a8744a6fe7d910ac
TeddyBEARs56 <apples5060@gmail.com>
2022-04-29 21:47:49 +0900

    오픈채팅방

--- 카카오 인턴/오픈채팅방.js
+++ 카카오 인턴/오픈채팅방.js
@@ -13,25 +13,27 @@ function solution(record) {
         log: [],
         user: [],
         check: -1,
+        arr: []
     }
-    let arr;
+    
     for(let con of record){
-        arr = con.split(' ');
+        item.arr = con.split(' ');
 
-        if(arr[0] === 'Enter'){
-            item.user[arr[1]] = arr[2]
-            item.log.push({id: arr[1], commend: "님이 들어왔습니다."})
+        if(item.arr[0] === 'Enter'){
+            item.user[item.arr[1]] = item.arr[2]
+            item.log.push({id: item.arr[1], commend: "님이 들어왔습니다."})
         }
-        else if(arr[0] === 'Leave'){
-            item.log.push({id: arr[1], commend: "님이 나갔습니다."})
+        else if(item.arr[0] === 'Leave'){
+            item.log.push({id: item.arr[1], commend: "님이 나갔습니다."})
         }
-        else if(arr[0] ===  'Change'){
-            item.user[arr[1]] = arr[2]   
+        else if(item.arr[0] ===  'Change'){
+            item.user[item.arr[1]] = item.arr[2]   
         }
     }
 
     for(let commend of item.log){
         item.answer.push(item.user[commend.id] + commend.commend);
+        
     }
 
     return item.answer;
@@ -40,3 +42,5 @@ function solution(record) {
 console.log(solution(record));
 // ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
 // ["Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."]
+
+// 어째서인지 find 함수 쓰는것 보다 Map(Dictionary)로 찾는게 훨신 빠르게 처리된다.
